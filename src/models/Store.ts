c1b8cd9a28b7d42583cee8c05aec025ac65fd9e8
Shishin Nikita <nktshn06@gmail.com>
2018-11-21 22:51:48 +0600

    store interface edit

--- src/models/Store.ts
+++ src/models/Store.ts
@@ -1,10 +1,11 @@
 import { Task } from "./Task";
+import { Observable } from "rxjs";
+import { TaskResponse } from "./TaskResponse";
 
 export interface IStore {
-    getTasks(): any;
-    getTask(taskId: number): any
-    editTask(task: Task): any
-    createTask(task: Task): any
-    deleteTask(id: number): any
-
+    getTasks(): Observable<TaskResponse[] | null>;
+    getTask(taskId: number): Observable<TaskResponse | null>
+    editTask(task: Task): Observable<boolean>
+    createTask(task: Task): Observable<boolean>
+    deleteTask(id: number): Observable<boolean>
 }
